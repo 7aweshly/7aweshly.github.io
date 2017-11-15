65a97c85002de5326ee5ba1fc8e233ebb74e4bb3
SherryGuirguis <sherry98@aucegypt.edu>
2017-11-15 15:03:34 +0200

    face lift

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var $ = jQuery;
+
+
+// Count Down Timer
+var starting = (Math.round((new Date()).getTime() / 1000)).toString();
+
+$('.countdown').final_countdown({
+    start : starting, //Here use Milisecond. To convert your time you can go to this(https://currentmillis.com/) website. 
+    end   : 1542270952,
+    now : starting,
+seconds: {
+borderColor: 'RGB(255,192,0)',
+borderWidth: '4'
+},
+minutes: {
+    borderColor: 'RGB(255,192,0)',
+    borderWidth: '3'
+},
+hours: {
+    borderColor: 'RGB(255,192,0)',
+    borderWidth: '3'
+},
+days: {
+    borderColor: 'RGB(255,192,0)',
+    borderWidth: '3'
+}}, function() {
+});
+
+ 
+
